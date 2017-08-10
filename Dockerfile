ea10ca0989cb164151f0a31be0632946508e99b4
codeneomatrix <josuecevedo@gmail.com>
2017-08-09 22:42:29 -0500

    mi version de ejecucion

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,20 @@
-FROM python:3.5-alpine
+FROM python:3.5
 
 ADD . /app
 WORKDIR /app
 
 RUN pip install -U discord.py
-RUN pip install requests==2.11.1
+
+ENV JULIA_PATH /usr/local/julia
+ENV JULIA_VERSION 0.6.0
+
+RUN mkdir $JULIA_PATH
+
+RUN curl -sSL "https://julialang-s3.julialang.org/bin/linux/x64/${JULIA_VERSION%[.-]*}/julia-${JULIA_VERSION}-linux-x86_64.tar.gz" -o julia.tar.gz
+
+RUN tar -xzf julia.tar.gz -C $JULIA_PATH --strip-components 1
+RUN rm -rf /var/lib/apt/lists/* julia.tar.gz*
+#julia-903644385b
+ENV PATH $JULIA_PATH/bin:$PATH
+
 CMD python app.py
--- app.py
+++ app.py
@@ -28,15 +28,29 @@ async def unido(member : discord.Member):
     """Dice cuando se unio un miembro."""
     await bot.say('{0.name} se unio el {0.joined_at}'.format(member))
 
+#@bot.command()
+#async def ejecuta(comando:str):
+#    await bot.say('ejecutando {}'.format(comando))
+
+
 @bot.command()
-async def ejecuta(comando:str):
-    await bot.say('ejecutando {}'.format(comando))
+async def julia(*, texto: str):
+    import subprocess
+    a = subprocess.check_output(["julia","-e %s" % texto])
+    await bot.say(a)
 
+# @bot.command()
+# async def julia(*commands:str):                                       
+#         try:                                                          
+#             output = commands.getstatus('julia -e {command}')
+#             await bot.say(output)                                        
+#         except error:                                                 
+#             await bot.say(error)   
 
-    # elif message.content.startswith('!acevedo'):
-    #     import requests
-    #     r = requests.post("https://neomatrix.herokuapp.com/graphql", data={'query': '{ neomatrix{ nombre email } }'})
-    #     await client.send_message(message.channel, 'los datos son {} '.format(r.text))
+# async with aiohttp.get('http://random.cat/meow') as r:
+#     if r.status == 200:
+#         js = await r.json()
+#         await client.send_message(channel, js['file'])
 
 
 bot.run("MzQ0OTIzODQzNjE1MzkxNzQ1.DG0PBQ.F5JANrRjCIvpenMfzmLBisYxaXo")
